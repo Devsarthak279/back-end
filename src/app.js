c0b6e4f8278308833d0b41454c8de1e2ec920367
Sarthak <nucleosarthak279@gmail.com>
2025-01-15 19:55:03 +0530

    configuration setup done

--- src/app.js
+++ src/app.js
@@ -12,6 +12,19 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+
+//routes import
+import userRouter from './routes/user.routers.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+// app.get('/',(req,res)=>{
+//     res.send("ello")
+// })
+
+
+
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 //require('dotenv').config({path:'/env'})
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
-    path:'/env'
+    path:'./.env'
 })
-
+//app.use("/api/v1/users", userRouter)
 
 connectDB()
 .then(()=>{
